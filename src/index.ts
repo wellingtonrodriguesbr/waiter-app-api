a9d1a5590ba5e87c03dda49f95bc834788bcc46d
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-11-16 07:26:05 -0300

    feat: add static route

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
+import path from "node:path";
 import { router } from "./Router";
 
 mongoose
   .connect("mongodb://localhost:27017")
   .then(() => {
     const app = express();
+    app.use(
+      "/uploads",
+      express.static(path.resolve(__dirname, "..", "uploads"))
+    );
     app.use(express.json());
     app.use(router);
     app.listen(3333, () => console.log("Server is running!"));
