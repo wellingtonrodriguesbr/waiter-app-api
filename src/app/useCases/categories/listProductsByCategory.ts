0e41d75643272afa2b3eb2c761a529672bcb582d
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-11-16 07:26:52 -0300

    feat: create methods of products

---
+++ src/app/useCases/categories/listProductsByCategory.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+import { Product } from "../../models/Product";
+
+export async function listProductsByCategory(req: Request, res: Response) {
+  const { categoryId } = req.params;
+  const products = await Product.find().where("category").equals(categoryId);
+
+  res.json(products);
+}
