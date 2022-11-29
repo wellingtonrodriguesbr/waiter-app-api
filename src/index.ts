c58ac855973c666c7653f196b34d9ea7bdcbc650
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-11-29 06:04:40 -0300

    feat: add websocket

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import "dotenv/config";
+import path from "node:path";
+import http from "node:http";
 import express from "express";
 import mongoose from "mongoose";
-import path from "node:path";
+import { Server } from "socket.io";
 import { router } from "./Router";
 
 const MONGODB_URI = process.env.MONGO_DB;
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
 
 mongoose
   .connect(MONGODB_URI || "")
   .then(() => {
-    const app = express();
     app.use((req, res, next) => {
       res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
       res.setHeader("Access-Control-Allow-Methods", "*");
@@ -22,8 +26,10 @@ mongoose
       "/uploads",
       express.static(path.resolve(__dirname, "..", "uploads"))
     );
+
     app.use(express.json());
     app.use(router);
-    app.listen(3333, () => console.log("Server is running!"));
+
+    server.listen(3333, () => console.log("Server is running!"));
   })
-  .catch(() => console.log("erro ao conectar no mongodb"));
+  .catch((err) => console.log(err));
